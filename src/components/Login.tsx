dd66cd6a9e2902faae35e6943c6ada8729331d1e
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-10-24 19:55:29 +0200

    Keylogger :checkmark: and you can now log in with RFID
    Using a card even (I hope).

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -20,10 +20,21 @@ const mapDispatchToProps = (
 });
 
 class LoginComponent extends React.Component<IDispatchFromProps> {
+    private buffer: string[];
+    private intervalID: any; /* This is actually a number, or a TimerHandler, but react can't find TimerHandler */
+    private intervalHandler: () => void;
+    private INTERVAL_CYCLE_DELAY: number;
+
     constructor(props: IDispatchFromProps) {
         super(props);
 
         this.keyListener = this.keyListener.bind(this);
+
+        this.INTERVAL_CYCLE_DELAY = 2000;
+        this.intervalHandler = () => { this.buffer = []; };
+
+        this.buffer = [];
+        this.intervalID = setInterval(this.intervalHandler, this.INTERVAL_CYCLE_DELAY);
     }
 
     public render() {
@@ -43,9 +54,15 @@ class LoginComponent extends React.Component<IDispatchFromProps> {
     }
 
     private keyListener(e: KeyboardEvent) {
+        clearInterval(this.intervalID);
+
         if (e.keyCode === 13) {
-            this.props.signIn("");
+            this.props.signIn(this.buffer.join(""));
+        } else {
+            this.buffer.push(e.key);
         }
+
+        this.intervalID = setInterval(this.intervalHandler, this.INTERVAL_CYCLE_DELAY);
     }
 }
 
