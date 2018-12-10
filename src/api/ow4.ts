bf80c3d0a100866b42276c1ab864742b7786cfe2
oleast <oleast@stud.ntnu.no>
2018-12-10 20:27:37 +0100

    Fix last mismatched amount

--- src/api/ow4.ts
+++ src/api/ow4.ts
@@ -22,7 +22,7 @@ function prepareBasket(basket: IBasketItem[]): IAPIBasketItem[] {
     const counted = countBasketItems(basket);
 
     return Object.keys(counted)
-        .map((key) => ({ object_id: key, amount: counted[key] }));
+        .map((key) => ({ object_id: key, quantity: counted[key] }));
 }
 
 export default {
